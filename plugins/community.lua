402aaaf66a1b44a73c3abc3150084b1e1446a7e9
Gerardo Gallegos <puxerop@gmail.com>
2024-02-02 19:52:21 -0600

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,5 +6,5 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.color.twilight.nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
 }
