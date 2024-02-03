66e372e86dc10f2e8142dd2e6ee5bad39552b398
Gerardo Gallegos <puxerop@gmail.com>
2024-02-02 19:37:25 -0600

    Adding emmet-ls configuration for js jsx etc in init.lua

--- init.lua
+++ init.lua
@@ -52,6 +52,19 @@ return {
     servers = {
       -- "pyright"
     },
+    config = {
+      emmet_ls = {
+        filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        }
+      }
+    }
   },
 
   -- Configure require("lazy").setup() options
